6718af38d1857fcf1e0b58d15ad0db5d3215f9c6
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-03-18 13:24:15 +0200

    feat: :sparkles: routes to various pages have been added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import MangaDetails from './modules/manga-details/components/MangaDetails';
+import Chapter from './modules/chapter/components/Chapter';
+import ErrorPage from './modules/error-page/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Placeholder</div>,
+  },
+  {
+    path: "view-manga/:selectedSlug",
+    element: <MangaDetails />
+  },
+  {
+    path: "view-manga/:selectedSlug/:chapterHID",
+    element: <Chapter />
+  },
+  {
+    path: "*",
+    element: <ErrorPage />
+  }
+]);
+
+const queryClient = new QueryClient()
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
