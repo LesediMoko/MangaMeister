2f57750ab3575beb91321a1b1f49a4010ef2a5b7
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-03-18 13:26:26 +0200

    feat: :sparkles: added api calls relating to comic and chapter information

---
+++ src/modules/common/services/api-service.ts
@@ -0,0 +1,35 @@
+import { IChapterInfoAPIResponse, IChaptersAPIResponse, IComicAPIResponse, ITopComicsAPIResponse } from "../types/api-types";
+
+const baseUrl = 'https://api.comick.io';
+
+export const fetchTrendingMangas = async (): Promise<ITopComicsAPIResponse> => {
+    const response = await fetch(`${baseUrl}/top?comic_types=manga&accept_mature_content=false`);
+    if (!response.ok) {
+        throw new Error('Network call failed');
+    }
+    return response.json();
+};
+
+export const fetchComicInfo = async (slug: string): Promise<IComicAPIResponse> => {
+    const response = await fetch(`${baseUrl}/comic/${slug}`);
+    if (!response.ok) {
+        throw new Error('Network call failed');
+    }
+    return response.json();
+};
+
+export const fetchChapterListInfo = async (hid: string, chapterTotal: number): Promise<IChaptersAPIResponse> => {
+    const response = await fetch(`${baseUrl}/comic/${hid}/chapters?limit=${chapterTotal}&lang=en`);
+    if (!response.ok) {
+        throw new Error('Network call failed');
+    }
+    return response.json();
+};
+
+export const fetchChapterInfo = async (hid: string): Promise<IChapterInfoAPIResponse> => {
+    const response = await fetch(`${baseUrl}/chapter/${hid}`);
+    if (!response.ok) {
+        throw new Error('Network call failed');
+    }
+    return response.json();
+};
