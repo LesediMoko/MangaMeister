8f9b0b0602bc2c2679c3d07076bf6815415ff630
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-03-18 13:30:01 +0200

    refactor: :art: types and store moved to common folder

--- src/services/api-service.ts
+++
@@ -1,9 +0,0 @@
-import { ITopComicsAPIResponse } from "../types/api-types";
-
-export const fetchTrendingMangas = async (): Promise<ITopComicsAPIResponse> => {
-    const response = await fetch('https://api.comick.io/top?comic_types=manga&accept_mature_content=false');
-    if (!response.ok) {
-        throw new Error('Network call failed');
-    }
-    return response.json();
-};
