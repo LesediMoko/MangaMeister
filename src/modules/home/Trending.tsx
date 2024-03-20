6f747e36f104dfd806f2be20370fcfebae303c9d
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-03-20 06:02:25 +0200

    feat: :sparkles: New features on Home page

--- src/modules/home/Trending.tsx
+++ src/modules/home/Trending.tsx
@@ -3,6 +3,7 @@ import { ITrendingMangaApp } from "../common/types/app-types";
 import { ITrendingManga } from "../common/types/subtypes";
 import useMangaStore from '../common/stores/store'
 import { fetchTrendingMangas } from "../common/services/api-service";
+import { Link } from "react-router-dom";
 
 function Trending() {
 
@@ -32,18 +33,20 @@ function Trending() {
     }
 
     return (
-        <div id="carousel-container" className="">
-            <strong className="text-left">Trending:</strong>
-            <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box">
-                {useMangaStore.getState().trendingMangas.map((manga: ITrendingMangaApp) => {
-                    return (
-                        <div key={manga.slug} className="carousel-item h-80 w-full">
-                            <img src={manga.coverImage} className="rounded-box" />
-                            {manga.title}
+        <div className="carousel carousel-end bg-black h-1/3 m-2">
+            {useMangaStore.getState().trendingMangas.map((manga: ITrendingMangaApp) => {
+                return (
+                    <div className="card w-full bg-base-100 shadow-xl image-full carousel-item h-full image-full:before:rounded-none">
+                        <figure className="w-full"><img src={manga.coverImage} alt={`Trending Manga: ${manga.title}`} className="w-full object-top" /></figure>
+                        <div className="card-body w-full">
+                            <h2 className="card-title w-full place-self-center inset-0">{manga.title}</h2>
+                            <div className="card-actions justify-end">
+                                <Link to={`view-manga/${manga.slug}`} className="btn btn-primary">Read</Link>
+                            </div>
                         </div>
-                    )
-                })}
-            </div>
+                    </div>
+                )
+            })}
         </div>
     )
 }
