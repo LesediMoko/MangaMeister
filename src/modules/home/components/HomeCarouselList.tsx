f45855f0e1e7153fc928d48f218431e7c2a9815b
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-03-21 00:57:26 +0200

    style: :fire: removal of unnecessary classes regard text ellipse

--- src/modules/home/components/HomeCarouselList.tsx
+++ src/modules/home/components/HomeCarouselList.tsx
@@ -8,7 +8,7 @@ const HomeCarouselList = ({ slug, title, coverImage }: ITrendingMangaApp) => {
                 <div className="card shadow-xl rounded-none gap-1 h-full">
                     <figure className="h-4/5 w-full"><img src={coverImage} alt={title} className="w-full h-full image-full object-cover" /></figure>
                     <div className="card-body h-1/6 p-0">
-                        <h2 className="card-title text-xs text-slate-50 text-clip text-ellipsis line-clamp-2">{title}</h2>
+                        <h2 className="card-title text-xs text-slate-50 line-clamp-2">{title}</h2>
                     </div>
                 </div>
             </Link>
