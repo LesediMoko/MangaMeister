e0a83d2842744ffc2aa4e0481b27325cd674f406
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-03-21 00:54:03 +0200

    feat: :sparkles: name and logo added to home

---
+++ src/modules/home/components/NameAndLogo.tsx
@@ -0,0 +1,12 @@
+const NameAndLogo = () => {
+    return (
+        <div className="flex flex-row h-20 place-self-center mb-4 mt-4">
+            <img src="/logo.webp" alt="App Logo" className="object-cover w-24 justify-around" />
+            <strong className="text-slate-100 place-self-center text-3xl">MangaMeister</strong>
+
+        </div>
+    )
+
+}
+
+export default NameAndLogo
