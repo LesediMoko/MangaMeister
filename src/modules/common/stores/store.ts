cfa6a29c85c0db0827203f2eb1e86017afc80834
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-03-24 05:09:14 +0200

    feat: :zap: store updated for persistance in local storage

--- src/modules/common/stores/store.ts
+++ src/modules/common/stores/store.ts
@@ -1,7 +1,25 @@
 import { create } from "zustand";
 import { MangaReaderState } from "../types/app-types";
+import { persist, createJSONStorage } from 'zustand/middleware'
 
-export const useMangaStore = create<MangaReaderState>(() => ({
+
+// export const useMangaStore = create<MangaReaderState>(() => ({
+//     mangas: [],
+//     selectedChapter: null,
+//     chapters: [],
+//     imageUrlPrefix: "https://meo3.comick.pictures/",
+//     trendingMangas: [],
+//     selectedManga: null,
+//     newMangas: [],
+//     popularMangas: [],
+//     topRankedMangas: [],
+//     scrollPosition: 0
+// }));
+
+// const storage = createJSONStorage<MangaReaderState>(() => localStorage)
+
+export const useMangaStore = create<MangaReaderState>()(persist((_set, _get) =>
+({
     mangas: [],
     selectedChapter: null,
     chapters: [],
@@ -10,7 +28,9 @@ export const useMangaStore = create<MangaReaderState>(() => ({
     selectedManga: null,
     newMangas: [],
     popularMangas: [],
-    topRankedMangas: []
-}));
+    topRankedMangas: [],
+    scrollPosition: { chapterID: "", position: 0 }
+}),
+    { name: 'manga-reader-store', storage: createJSONStorage(() => localStorage) }));
 
 export default useMangaStore;
