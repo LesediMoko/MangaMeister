f042b20ed93a116d9e4aafb9965ec9864425c147
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-03-24 05:06:29 +0200

    feat: :wrench: updated tailwind config file to include custom colors and logos for dark and light themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	darkMode: "media",
 	theme: {
-		extend: {},
+		colors: {
+			"light-secondary": "#4A0638",
+			"light-primary": "#FFFFFF",
+			"dark-primary": "#000000",
+			"dark-secondary": "#F6F4F1",
+		},
+		extend: {
+			backgroundImage: () => ({
+				"logo-light": "url('/logo-light.svg')",
+				"logo-dark": "url('/logo-dark.svg')",
+			}),
+		},
 	},
+	variants: {
+		extend: { backgroundImage: ["dark"] },
+	},
+
 	plugins: [require("daisyui")],
 };
