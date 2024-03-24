768a6274ef3e35f3350fcb2c50d680fb6bf4c839
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-03-24 05:15:07 +0200

    feat: :lipstick: themed app logo added to home page

--- src/modules/home/components/NameAndLogo.tsx
+++ src/modules/home/components/NameAndLogo.tsx
@@ -1,12 +1,11 @@
 const NameAndLogo = () => {
     return (
         <div className="flex flex-row h-20 place-self-center mb-4 mt-4">
-            <img src="/logo.webp" alt="App Logo" className="object-cover w-24 justify-around" />
+            <div className="bg-logo-light dark:bg-logo-dark w-24 justify-around bg-center bg-cover"></div>
             <strong className="text-slate-100 place-self-center text-3xl">MangaMeister</strong>
 
         </div>
     )
-
 }
 
 export default NameAndLogo
