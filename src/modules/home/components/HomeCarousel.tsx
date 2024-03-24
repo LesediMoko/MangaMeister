6fc37ab942e7d51b58c9960b5a59038bd917dbca
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-03-24 05:13:57 +0200

    feat: :sparkles: "see all" option added to end of carousel

--- src/modules/home/components/HomeCarousel.tsx
+++ src/modules/home/components/HomeCarousel.tsx
@@ -4,6 +4,7 @@ import { getCarouselDetails, getHomeCarouselState, setHomeCarouselState } from "
 import HomeCarouselList from "./HomeCarouselList"
 import { carouselFilter } from "../../common/helpers/response-filters"
 import CarouselSkeleton from "./CarouselSkeleton"
+import { Link } from "react-router-dom"
 
 const HomeCarousel = ({ category }: { category: string }) => {
     const { heading, queryKey } = getCarouselDetails(category)
@@ -25,15 +26,24 @@ const HomeCarousel = ({ category }: { category: string }) => {
         setHomeCarouselState(category, newMangaListState)
     }
     return (
-        <div className="flex flex-col h-80 ml-2 mr-2 mb-10">
+        <div className="flex flex-col ml-2 mr-2">
             <div className="flex flex-row justify-between mb-2 ml-2 mr-2 text-slate-50">
                 <p>{heading}</p>
-                <p>...</p>
+                <Link to={`view-all/${category}`} state={{ previousPage: "Home" }} ><p>...</p></Link>
             </div>
-            <div className="carousel gap-2">
-                {getHomeCarouselState(category).map((manga) => {
+            <div className="carousel gap-2 mb-5">
+                {getHomeCarouselState(category).slice(0, 8).map((manga) => {
                     return (<HomeCarouselList coverImage={manga.coverImage} slug={manga.slug} title={manga.title} key={manga.slug} />)
                 })}
+                <div className="carousel-item w-1/3 overflow-y-hidden flex flex-col place-content-center text-center">
+                    <Link to={`view-all/${category}`} state={{ previousPage: "Home" }}>
+                        <div className="card  rounded-none gap-1 h-full  ">
+                            <div className="card-body h-1/6 p-0">
+                                <p className="card-title text-2xl text-slate-50 line-clamp-2">See All</p>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
             </div>
         </div>
     )
