16e68f278c85be399e6550fb29e1e4d252c1f312
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-03-24 05:16:32 +0200

    feat: :lipstick: card removed in favour of div with background image for carousel

--- src/modules/home/components/TrendingList.tsx
+++ src/modules/home/components/TrendingList.tsx
@@ -3,13 +3,11 @@ import { ITrendingMangaApp } from "../../common/types/app-types";
 
 const TrendingList = ({ slug, title, coverImage }: ITrendingMangaApp) => {
     return (
-        <div className="card w-full bg-base-100 shadow-xl image-full carousel-item h-full">
-            <figure className="w-full rounded-none"><img src={coverImage} alt={`Trending Manga: ${title}`} className="rounded-none w-full object-top" /></figure>
-            <div className="card-body w-full rounded-none justify-between">
-                <h2 className="card-title w-full text-center inset-0">{title}</h2>
-                <div className="card-actions justify-end">
-                    <Link to={`view-manga/${slug}`} className="btn btn-primary">Read</Link>
-                </div>
+        <div className="w-full md:w-1/2 lg:w-1/3 shadow-xl carousel-item flex flex-col gap-10 justify-center h-full relative items-center text-center bg-cover" style={{ backgroundImage: `url('${coverImage}')`, backgroundPosition: "top", backgroundRepeat: "no-repeat" }}>
+            <div className="w-full shadow-xl h-full absolute z-10 bg-dark-primary opacity-65"></div>
+            <h2 className="card-title w-full text-center text-2xl ml-6 mr-6 line-clamp-2 z-20 items-center justify-center text-light-primary">{title}</h2>
+            <div className="card-actions">
+                <Link to={`view-manga/${slug}`} className="btn bg-light-primary text-light-secondary z-20 text-lg" state={{ previousPage: "Home" }}>Read</Link>
             </div>
         </div>
 
