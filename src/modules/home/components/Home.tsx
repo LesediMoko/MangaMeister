a937f7bc32d66d55503e741fe67a644c6a8ec51d
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-03-24 05:11:00 +0200

    feat: :lipstick: added text and bg colors for themes

--- src/modules/home/components/Home.tsx
+++ src/modules/home/components/Home.tsx
@@ -4,7 +4,7 @@ import Trending from "./Trending"
 
 const Home = () => {
     return (
-        <div className="flex flex-col h-full bg-black">
+        <div className="flex flex-col h-full bg-light-primary dark:bg-dark-primary text-light-secondary dark:text-dark-secondary">
             <NameAndLogo />
             <Trending />
             <HomeCarousel category="new" />
