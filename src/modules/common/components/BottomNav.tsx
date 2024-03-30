5eaef9c9882ad48388b5547fdd5ba3b4180db219
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-03-30 18:43:59 +0200

    feat: :sparkles: added Discover tab to bottom navigation

--- src/modules/common/components/BottomNav.tsx
+++ src/modules/common/components/BottomNav.tsx
@@ -1,17 +1,21 @@
 import { GiArchiveResearch } from "react-icons/gi";
 import { BiHomeSmile } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
 const BottomNav = ({ activePage }: { activePage: string }) => {
     return (
-        <div className="btm-nav">
+        <div className="btm-nav mb-0">
             <button className={activePage == "Home" ? "active" : ""}>
-                <BiHomeSmile className="size-5" />
-                <span className="btm-nav-label">Home</span>
+                <Link to="/" state={{ previousPage: "Home" }} className="flex flex-col items-center">
+                    <BiHomeSmile className="size-5" />
+                    <span className="btm-nav-label">Home</span>
+                </Link>
             </button>
             <button className={activePage == "Discover" ? "active" : ""}>
-
-                <GiArchiveResearch className="size-5" />
-                <span className="btm-nav-label">Discover</span>
+                <Link to="/discover" state={{ previousPage: "Discover" }} className="flex flex-col items-center">
+                    <GiArchiveResearch className="size-5" />
+                    <span className="btm-nav-label">Discover</span>
+                </Link>
             </button>
         </div>
     )
