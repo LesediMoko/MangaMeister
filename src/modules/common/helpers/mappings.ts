3e851c8f1672282f7833fcbe1ec7aa1439e60bbf
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-03-30 18:47:09 +0200

    feat: :sparkles: introduced a filter mapper to use appropriate useQuery and payload

--- src/modules/common/helpers/mappings.ts
+++ src/modules/common/helpers/mappings.ts
@@ -1,4 +1,5 @@
 import { IHomeCarouselData } from "../../home/types/home-types";
+import { fetchGenreFilterResults, fetchStatusFilterResults } from "../services/api-service";
 import useMangaStore from "../stores/store";
 import { ITrendingMangaApp } from "../types/app-types";
 
@@ -71,6 +72,22 @@ export const setHomeCarouselState = (category: string, newState: ITrendingMangaA
             break;
         case 'popular':
             useMangaStore.setState({ popularMangas: newState })
+            break;
+        default:
+            break;
+    }
+}
+
+export const getFilterRequestAndPayload = (filterType: string): { apiRequest: (arg0: number | string[] | null) => void, payload: null | number | string[] } | undefined => {
+    switch (filterType) {
+        case "genre":
+            return { apiRequest: (genres: number | string[] | null) => fetchGenreFilterResults(genres as string[]), payload: useMangaStore.getState().selectedGenres }
+            break;
+        case "status":
+            return { apiRequest: (status: number | string[] | null) => fetchStatusFilterResults(status as number), payload: useMangaStore.getState().selectedStatus }
+            break;
+        case "":
+            return { apiRequest: () => { }, payload: null }
             break;
         default:
             break;
