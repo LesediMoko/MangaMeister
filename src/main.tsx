1edbabda99c3d92bead5381c1b4106e35447e7cd
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-04-02 00:04:59 +0200

    style: :fire: removed commented out routes

--- src/main.tsx
+++ src/main.tsx
@@ -7,50 +7,7 @@ import {
 } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import LazySuspenseWrapper from './modules/common/components/LazySuspenseWrapper';
-// import MangaDetails from './modules/manga-details/components/MangaDetails';
-// import Chapter from './modules/chapter/components/Chapter';
-// import ErrorPage from './modules/error-page/ErrorPage';
-// import Home from './modules/home/components/Home';
-// import ViewAll from './modules/view-all/ViewAll';
-// import Discover from './modules/discover/components/Discover';
-// import LoadErrorPage from './modules/error-page/LoadErrorPage';
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Home />,
-//   },
-//   {
-//     path: "view-manga/:selectedSlug",
-//     element: <MangaDetails />
-//   },
-//   {
-//     path: "view-manga/:selectedSlug/:chapterHID",
-//     element: <Chapter />
-//   },
-//   {
-//     path: "view-all/:category",
-//     element: <ViewAll />
-//   },
-//   {
-//     path: "discover",
-//     element: <Discover />
-//   },
-//   {
-//     path: "discover/status/:status",
-//     element: <Discover />
-//   },
-//   {
-//     path: "load-error",
-//     element: <LoadErrorPage />
-//   },
-//   {
-//     path: "*",
-//     element: <ErrorPage />
-//   }
-// ]);
-
-// Convert static imports to dynamic imports using React.lazy
 const MangaDetails = React.lazy(() => import('./modules/manga-details/components/MangaDetails'));
 const Chapter = React.lazy(() => import('./modules/chapter/components/Chapter'));
 const Home = React.lazy(() => import('./modules/home/components/Home'));
