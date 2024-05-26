e11a97ef94c58504cbce478752c8b93fb2872e3d
Lesedi Mathabatha Mokonyama <93593911+LesediMoko@users.noreply.github.com>
2024-05-26 06:20:20 +0200

    feat: :sparkles: added onClick event to change orientation for reading

--- src/modules/common/components/NavBar.tsx
+++ src/modules/common/components/NavBar.tsx
@@ -1,7 +1,21 @@
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useLocation, useNavigate } from "react-router-dom"
+import { CgScrollH, CgScrollV } from "react-icons/cg";
+import useMangaStore from "../stores/store";
 
-const NavBar = ({ previousPage }: { previousPage: string }) => {
+const NavBar = ({ previousPage} : { previousPage: string, onOrientationClick? : (orientation : string | undefined)=> void}) => {
     const navigate = useNavigate()
+    const location = useLocation()
+    const orientationValue = useMangaStore(state => state.orientation)
+    const handleOrientationChange = () => {
+        console.log("orientation clicked")
+        console.log(`old orientation value: ${orientationValue}`)
+        if(orientationValue === "vertical"){
+            useMangaStore.setState({orientation: "horizontal"})
+        }else{
+            useMangaStore.setState({orientation: "vertical"})
+        }
+        console.log(`new orientation value: ${orientationValue}`)
+    }
     return (
         <div className="navbar  w-full place-self-center fixed z-20 top-0 bg-light-primary dark:bg-dark-primary">
             <div className="navbar-start">
@@ -19,7 +33,12 @@ const NavBar = ({ previousPage }: { previousPage: string }) => {
                 <a className="btn btn-ghost text-xl">MangaMeister</a>
             </div>
             <div className="navbar-end">
+                {location.pathname.match(/view-manga\/[^\\/]+\/[^\\/]+/) ? 
+                orientationValue === "vertical" ?
+                <CgScrollV className="h-8 w-8 opacity-65 rounded-full" onClick={handleOrientationChange}/> :
+                <CgScrollH className="h-8 w-8 opacity-65 rounded-full" onClick={handleOrientationChange}/> :
                 <img src="/icon.webp" alt="icon" className="h-8 w-8 opacity-65 rounded-full" />
+                }
             </div>
         </div>
     )
